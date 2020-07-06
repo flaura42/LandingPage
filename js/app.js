c22b8ba82bcf5ea90f65c7df89d2549f06015da8
Laura <flaura42@gmail.com>
2020-07-06 03:04:06 -0700

    fix nav to not use anchor scrolling. add active nav/section color changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//----- event listeners -----//
+
 // Wait for page to load
 document.addEventListener('DOMContentLoaded', () => {
   addSections(8);
@@ -5,24 +7,55 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 // make nav li clickable
-const nav = document.getElementById('nav-list');
-nav.addEventListener('click', (items) => {
-  const item = items.target.firstChild.innerText;
-  location.href = `#section-${item}`;
-  activeSection(item);
+document.getElementById('nav-list').addEventListener('click', (event) => {
+  console.log('been clicked, yo');
+  const id = event.target.firstChild.innerText;
+  scrollLink(id);
 })
 
+//----- functions -----//
+
+// FIXME: Adjust 'top' to work going up and down.
+// scroll window to selected section
+function scrollLink(id) {
+  console.log('scroll to', id);
+  const dest = document.getElementById(`section-${id}`);
+  const top = dest.getBoundingClientRect().y + window.scrollY - 116;
+  window.scrollTo({
+    top,
+    behavior: 'smooth'
+  });
+  activeSection(dest, id);
+}
+
 // change active section
-function activeSection(item) {
+function activeSection(dest, id) {
+  console.log(dest);
   const sections = document.getElementsByTagName('section');
-  for (const sect in sections) {
-    const item = sections[sect];
-    if (item.className == 'active-section') {
-      item.classList.remove('active-section');
+  const navs = document.getElementsByTagName('li');
+  const links = document.getElementsByTagName('a');
+  for (let i in sections) {
+    const sect = sections[i];
+    if (sect.className == 'active-section') {
+      sect.classList.remove('active-section');
+    }
+    for (let i in navs) {
+      if (navs[i].className == 'active-nav') {
+        navs[i].classList.remove('active-nav');
+      }
+    }
+    for (let i in links) {
+      if (links[i].className == 'active-link') {
+        links[i].classList.remove('active-link');
+      }
     }
   }
-  const section = document.getElementById(`section-${item}`);
-  section.className = 'active-section'
+  const nav = navs[id - 1];
+  const link = navs[id - 1].firstChild;
+  console.log('testing', navs[id - 1].firstChild);
+  dest.className = 'active-section';
+  nav.className = 'active-nav';
+  link.className = 'active-link';
 }
 
 // add sections to the page
@@ -40,6 +73,10 @@ function addSections(num) {
     // create sections
     const section = document.createElement('section');
     section.id = `section-${i}`;
+    // give first section active class
+    if (i === 1) {
+      section.className = 'active-section';
+    }
     section.setAttribute('style', `background-color: ${color};`);
     section.setAttribute('data-section', i);
     const p = document.createElement('p');
@@ -60,7 +97,17 @@ function fillNav() {
   for (const section of sections) {
     const li = document.createElement('li');
     const a = document.createElement('a');
+    // give first nav active class
+    if (section.id === 'section-1') {
+      li.className = 'active-nav';
+      a.className = 'active-link'
+    }
     a.href = `#${section.id}`;
+    a.addEventListener('click', (event) => {
+      console.log('hear ya', event.target.innerText);
+      event.preventDefault();
+      scrollLink(event.target.innerText);
+    })
     a.innerText = section.dataset.section;
     li.appendChild(a);
     fragment.appendChild(li);
